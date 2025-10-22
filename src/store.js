14c09b57821e975f36eb936e154555b243b6478c
niramay123 <146821073+niramay123@users.noreply.github.com>
2025-10-22 13:04:09 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,19 +1,19 @@
 ```javascript
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './App';
-import './App.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import productReducer from './reducers/productReducer';
+import cartReducer from './reducers/cartReducer';
 
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-<Provider store={store}>
-<App />
-</Provider>
-);
+const rootReducer = combineReducers({
+products: productReducer,
+cart: cartReducer
+});
+
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export default store;
 ```
 
 
