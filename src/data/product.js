035e80944f1b79cde769db60ed75d60197006a9e
niramay123 <146821073+niramay123@users.noreply.github.com>
2025-10-22 13:06:24 +0530

    Create product.js

---
+++ src/data/product.js
@@ -0,0 +1,16 @@
+```javascript
+const PRODUCTS = [
+{ id: 'p1', name: 'Snake Plant', price: 499, category: 'Low Light', thumbnail: 'https://i.imgur.com/7bI4Q2k.png' },
+{ id: 'p2', name: 'Fiddle Leaf Fig', price: 1499, category: 'Bright Light', thumbnail: 'https://i.imgur.com/3QGQz5K.png' },
+{ id: 'p3', name: 'ZZ Plant', price: 699, category: 'Low Light', thumbnail: 'https://i.imgur.com/ZQZzV0K.png' },
+{ id: 'p4', name: 'Monstera', price: 1299, category: 'Bright Light', thumbnail: 'https://i.imgur.com/1Q9Z1ZB.png' },
+{ id: 'p5', name: 'Pothos', price: 299, category: 'Easy Care', thumbnail: 'https://i.imgur.com/2nCt3Sbl.png' },
+{ id: 'p6', name: 'Peace Lily', price: 799, category: 'Easy Care', thumbnail: 'https://i.imgur.com/4AiXzf8.png' }
+];
+
+
+export default PRODUCTS;
+```
+
+
+---
