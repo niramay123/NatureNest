3409e830fa1c13af921cdfb2074952d348ecc023
niramay123 <146821073+niramay123@users.noreply.github.com>
2025-10-22 13:08:13 +0530

    Create ProductListing.js

---
+++ src/components/ProductListing.js
@@ -0,0 +1,43 @@
+```javascript
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import ProductCard from './ProductCard';
+import { addToCart } from '../actions/cartActions';
+
+
+export default function ProductListing() {
+const products = useSelector((s) => s.products.items);
+const cartItems = useSelector((s) => s.cart.items);
+const dispatch = useDispatch();
+
+
+const categories = [...new Set(products.map(p => p.category))];
+
+
+return (
+<div className="product-listing">
+<h2>Plants for Sale</h2>
+<div className="categories">
+{categories.map(cat => (
+<div key={cat} className="category-block">
+<h3>{cat}</h3>
+<div className="grid">
+{products.filter(p => p.category === cat).map(p => (
+<ProductCard
+key={p.id}
+product={p}
+onAdd={(product) => dispatch(addToCart(product))}
+disabled={!!cartItems[p.id]}
+/>
+))}
+</div>
+</div>
+))}
+</div>
+</div>
+);
+}
+```
+
+
+---
