2aa2d62f4a3cbb2044255591bed7f2c912fea543
niramay123 <146821073+niramay123@users.noreply.github.com>
2025-10-22 13:05:01 +0530

    Create productReducer.js

---
+++ src/reducers/productReducer.js
@@ -0,0 +1,19 @@
+```javascript
+import PRODUCTS from '../data/products';
+
+
+const initialState = {
+items: PRODUCTS
+};
+
+
+export default function productReducer(state = initialState, action) {
+switch (action.type) {
+default:
+return state;
+}
+}
+```
+
+
+---
